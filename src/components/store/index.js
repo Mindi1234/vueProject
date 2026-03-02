517b762fa6782c3cbb6c3c1a60bfbeda934a5638
Mindi1234 <mimikenig1@gmail.com>
2026-03-02 11:56:51 +0200

    store and routs to the project

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -5,26 +5,57 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        users: [],
-        taska: []
+        users: [
+            { id: 1, name: "Alice", password: "password1" },
+            { id: 2, name: "Eden", password: "1234@" },
+            { id: 3, name: "Shira", password: "shira12$" },
+        ],
+        tasks: [
+        { id: 1, title: "h.w.", description: "do homework in math", status: false },
+        { id: 2, title: "clean", description: "do the dishes", status: false },
+        { id: 3, title: "bake", description: "make chocklete chips cookies", status: false }
+        ],
+        currentUser: JSON.parse(localStorage.getItem("currentUser")) || null,
     },
 
     mutations: {
+        login(state, payload) {
+            const user = state.users.find(
+                (u) => u.name === payload.name &&
+                u.password === payload.password
+            );
+            if (user) {
+                state.currentUser = user;
+                localStorage.setItem("currentUser", JSON.stringify(user));
+            } else {
+                alert("Invalid username or password");
+            }
+        },
+            logout(state) {
+                state.currentUser = null;
+                localStorage.removeItem("currentUser");
+            },
         setUsers(state, users) {
             state.users = users;
         },
-        setTaska(state, taska) {
-            state.taska = taska;
+        setTasks(state, tasks) {
+            state.tasks = tasks;
         }
 
     },
 
    getters: {
+        isLoggedIn(state) {
+            return !!state.currentUser;
+        },
+        currentUser(state) {
+            return state.currentUser;
+        },
         getUsers(state) {
             return state.users;
         },
-        getTaska(state) {
-            return state.taska;
+        getTasks(state) {
+            return state.tasks;
         }
     },
 
